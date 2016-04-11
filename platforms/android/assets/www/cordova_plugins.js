45892c164aea802610a8d2a1ba43847d7526ac56
mariobp <mariobarrpach@gmail.com>
2016-04-11 02:59:04 -0500

    Native Animation

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,15 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/com.telerik.plugins.nativepagetransitions/www/NativePageTransitions.js",
+        "id": "com.telerik.plugins.nativepagetransitions.NativePageTransitions",
+        "clobbers": [
+            "window.plugins.nativepagetransitions"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "Camera"
         ]
@@ -11,7 +17,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
         "id": "cordova-plugin-camera.CameraPopoverOptions",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "CameraPopoverOptions"
         ]
@@ -19,7 +24,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/Camera.js",
         "id": "cordova-plugin-camera.camera",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "navigator.camera"
         ]
@@ -27,7 +31,6 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
         "id": "cordova-plugin-camera.CameraPopoverHandle",
-        "pluginId": "cordova-plugin-camera",
         "clobbers": [
             "CameraPopoverHandle"
         ]
@@ -35,46 +38,20 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
         "clobbers": [
             "device"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "pluginId": "cordova-plugin-splashscreen",
+        "file": "plugins/cordova-plugin-image-picker/www/imagepicker.js",
+        "id": "cordova-plugin-image-picker.ImagePicker",
         "clobbers": [
-            "navigator.splashscreen"
+            "plugins.imagePicker"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
-        "runs": true
-    },
-    {
-        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
-        "id": "ionic-plugin-keyboard.keyboard",
-        "pluginId": "ionic-plugin-keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ],
-        "runs": true
     },
     {
         "file": "plugins/cordova-plugin-network-information/www/network.js",
         "id": "cordova-plugin-network-information.network",
-        "pluginId": "cordova-plugin-network-information",
         "clobbers": [
             "navigator.connection",
             "navigator.network.connection"
@@ -83,46 +60,63 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-network-information/www/Connection.js",
         "id": "cordova-plugin-network-information.Connection",
-        "pluginId": "cordova-plugin-network-information",
         "clobbers": [
             "Connection"
         ]
     },
     {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
         "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
         "id": "cordova-plugin-x-toast.Toast",
-        "pluginId": "cordova-plugin-x-toast",
         "clobbers": [
             "window.plugins.toast"
         ]
     },
     {
         "file": "plugins/cordova-plugin-x-toast/test/tests.js",
-        "id": "cordova-plugin-x-toast.tests",
-        "pluginId": "cordova-plugin-x-toast"
+        "id": "cordova-plugin-x-toast.tests"
     },
     {
-        "file": "plugins/cordova-plugin-image-picker/www/imagepicker.js",
-        "id": "cordova-plugin-image-picker.ImagePicker",
-        "pluginId": "cordova-plugin-image-picker",
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
         "clobbers": [
-            "plugins.imagePicker"
-        ]
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.telerik.plugins.nativepagetransitions": "0.6.4",
     "cordova-plugin-camera": "2.1.1",
     "cordova-plugin-console": "1.0.2",
     "cordova-plugin-device": "1.1.1",
+    "cordova-plugin-image-picker": "1.0.8",
+    "cordova-plugin-network-information": "1.2.0",
     "cordova-plugin-splashscreen": "3.1.0",
     "cordova-plugin-statusbar": "2.1.2",
     "cordova-plugin-whitelist": "1.2.1",
-    "ionic-plugin-keyboard": "2.0.1",
-    "cordova-plugin-network-information": "1.2.0",
     "cordova-plugin-x-toast": "2.5.0",
-    "cordova-plugin-image-picker": "1.0.8"
-}
+    "ionic-plugin-keyboard": "2.0.1"
+};
 // BOTTOM OF METADATA
 });
--- www/js/app.js
+++ www/js/app.js
@@ -4,153 +4,199 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ngCordova','ionic-modal-select','ionic.service.core', 'starter.controllers'])
+angular.module('starter', ['ionic', 'ngCordova', 'ionic-modal-select', 'ionic.service.core', 'starter.controllers', 'ionic-native-transitions'])
 
 .run(function($ionicPlatform, $ionicPopup, $interval, $http, $location, $window) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-
-  });
-  document.addEventListener("offline", onOffline, false);
-  function onOffline() {
-      // Handle the offline event
-      $ionicPopup.confirm({
-          title: "Internet desconectado",
-          content: "Su equipo esta desconectado de Internet.",
-          cancelText:'Cerrar',
-          cancelType:'button-assertive'
-      }).then(function(result) {
-          if(!result) {
-              ionic.Platform.exitApp();
-          }
-      });
-  }
-
-  document.addEventListener("online", onOnline, false);
-  function onOnline() {
-    // Handle the online event
-    var server = "http://104.236.33.228:8040";
-
-    var isLogin = function(){
-      $http.get(server+"/is/login/")
-      .then(function doneCallbacks(response){
-        if($location.path()=="/app/login/0"){
-          $location.path('/app/clientelists');
         }
-      }, function failCallbacks(response){
-        if($location.path()!=="/app/login/0"){
-          $location.path("/app/login/0");
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
         }
-      });
-    };
 
-    $http.get(server+"/serve/on/").then(function doneCallbacks(response){
-        isLogin();
-    },function failCallbacks(response){
-      if (response.status===0) {
+    });
+    document.addEventListener("offline", onOffline, false);
+
+    function onOffline() {
+        // Handle the offline event
         $ionicPopup.confirm({
-            title: "Server",
-            content: "Servidor fuera de servicio",
-            cancelText:'Cerrar',
-            cancelType:'button-assertive'
+            title: "Internet desconectado",
+            content: "Su equipo esta desconectado de Internet.",
+            cancelText: 'Cerrar',
+            cancelType: 'button-assertive'
         }).then(function(result) {
-            if(!result) {
+            if (!result) {
                 ionic.Platform.exitApp();
             }
         });
-      }
-    });
+    }
 
-  }
+    document.addEventListener("online", onOnline, false);
+
+    function onOnline() {
+        // Handle the online event
+        var server = "http://104.236.33.228:8040";
+
+        var isLogin = function() {
+            $http.get(server + "/is/login/")
+                .then(function doneCallbacks(response) {
+                    if ($location.path() == "/app/login/0") {
+                        $location.path('/app/clientelists');
+                    }
+                }, function failCallbacks(response) {
+                    if ($location.path() !== "/app/login/0") {
+                        $location.path("/app/login/0");
+                    }
+                });
+        };
+
+        $http.get(server + "/serve/on/").then(function doneCallbacks(response) {
+            isLogin();
+        }, function failCallbacks(response) {
+            if (response.status === 0) {
+                $ionicPopup.confirm({
+                    title: "Server",
+                    content: "Servidor fuera de servicio",
+                    cancelText: 'Cerrar',
+                    cancelType: 'button-assertive'
+                }).then(function(result) {
+                    if (!result) {
+                        ionic.Platform.exitApp();
+                    }
+                });
+            }
+        });
+
+    }
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider, $ionicNativeTransitionsProvider) {
+    $ionicConfigProvider.views.transition('none');
+    $ionicNativeTransitionsProvider.setDefaultOptions({
+        duration: 300, // in milliseconds (ms), default 400,
+        slowdownfactor: 4, // overlap views (higher number is more) or no overlap (1), default 4
+        iosdelay: -1, // ms to wait for the iOS webview to update before animation kicks in, default -1
+        androiddelay: -1, // same as above but for Android, default -1
+        winphonedelay: -1, // same as above but for Windows Phone, default -1,
+        fixedPixelsTop: 0, // the number of pixels of your fixed header, default 0 (iOS and Android)
+        fixedPixelsBottom: 0, // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
+        triggerTransitionEvent: '$ionicView.afterEnter', // internal ionic-native-transitions option
+        backInOppositeDirection: false // Takes over default back transition and state back transition to use the opposite direction transition to go back
+    });
+    $ionicNativeTransitionsProvider.setDefaultTransition({
+        type: 'slide',
+        direction: 'left'
+    });
+    $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.login', {
-      url: '/login/:next',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/login2.html',
-          controller: 'Login'
-        }
-      }
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
     })
 
-  .state('app.reporte', {
-      url: '/reporte/:clienteId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/reporte.html',
-          controller: 'Reporte'
+    .state('app.login', {
+        url: '/login/:next',
+        nativeTransitionsAndroid: {
+          "type": "slider",
+          "direction": "up"
+        },
+        nativeTransitionsBackAndroid: {
+          "type": "slider",
+          "direction": "down"
+        },  
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/login2.html',
+                controller: 'Login'
+            }
         }
-      }
+    })
+
+    .state('app.reporte', {
+        url: '/reporte/:clienteId',
+         nativeTransitionsAndroid: {
+          "type": "slider",
+          "direction": "up"
+        },
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/reporte.html',
+                controller: 'Reporte'
+            }
+        }
     })
 
     .state('app.mantenimiento', {
-      url: '/mantenimiento/:clienteId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/mantenimiento.html',
-          controller:'Mantenimiento'
+        url: '/mantenimiento/:clienteId',
+        nativeTransitionsAndroid: {
+          "type": "slider",
+          "direction": "up"
+        },
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/mantenimiento.html',
+                controller: 'Mantenimiento'
+            }
         }
-      }
     })
 
     .state('app.reparacion', {
-      url: '/reparacion/:clienteId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/reparacion.html',
-          controller:'Reparacion'
+        url: '/reparacion/:clienteId',
+        nativeTransitionsAndroid: {
+          "type": "slider",
+          "direction": "up"
+        },
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/reparacion.html',
+                controller: 'Reparacion'
+            }
         }
-      }
     })
 
     .state('app.clientelists', {
-      url: '/clientelists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/Clientelists.html',
-          controller: 'Clientelists'
+        url: '/clientelists',
+        nativeTransitionsBackAndroid: {
+          "type": "slider",
+          "direction": "right"
+        },  
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/Clientelists.html',
+                controller: 'Clientelists'
+            }
         }
-      }
     })
 
-  .state('app.info', {
-    url: '/info/:clienteId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/InfoC.html',
-        controller: 'InfoC'
-      }
-    }
-  })
-  .state('app.acerca', {
-    url: '/acerca',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/acerca.html',
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/clientelists');
+    .state('app.info', {
+        url: '/info/:clienteId',
+        nativeTransitionsBackAndroid: {
+          "type": "slider",
+          "direction": "down"
+        },  
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/InfoC.html',
+                controller: 'InfoC'
+            }
+        }
+    })
+        .state('app.acerca', {
+            url: '/acerca',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/acerca.html',
+                }
+            }
+        });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/clientelists');
 });
