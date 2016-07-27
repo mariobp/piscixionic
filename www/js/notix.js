40d404ce425759059290f642f7dc12380bc40b86
Mario Barrios <mariobarrpach@gmail.com>
2016-07-26 23:44:06 -0500

    update notix

--- www/js/notix.js
+++ www/js/notix.js
@@ -15,6 +15,7 @@ angular.module('starter.socket', [])
         event: null,
         callback: null,
         notixList: [],
+        list_actual: [],
         data: {},
 
         setup: function(session_id, username, type) {
@@ -39,80 +40,85 @@ angular.module('starter.socket', [])
 
             scope.socket.on('notix', function(message) {
                 if (scope.recive) {
-                    this.notixList.push(message);
-                    scope.lista_id.push(message._id);
-                    var id_message = scope.lista_id.indexOf(message._id) + 1;
-                    if (message.data.data.tipo == "Reporte") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Reporte',
-                            text: message.data.html,
-                            data: {
-                                reporte: message.data.data.reporte_id,
-                                cliente: message.data.data.cliente_id,
-                                tipo: message.data.data.tipo
+                    if (message.data.data !== undefined) {
+                        this.notixList.push(message);
+                        scope.lista_id.push(message._id);
+                        this.list_actual = [];
+                        var id_message = scope.lista_id.indexOf(message._id) + 1;
+                        if (message.data.data.tipo == "Reporte") {
+                            if ($state.current.name == "app.historialR") {
+                                scope.$broadcast('leer', message.data.data);
+                            } else {
+                                $cordovaLocalNotification.schedule({
+                                    id: id_message,
+                                    title: 'Reporte',
+                                    text: message.data.html,
+                                    data: {
+                                        reporte: message.data.data.reporte_id,
+                                        cliente: message.data.data.cliente_id,
+                                        tipo: message.data.data.tipo
+                                    }
+                                });
                             }
-                        });
-                    } else if (message.data.data.tipo == "Actividad") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Actividad',
-                            text: message.data.html,
-                            data: {
-                                tipo: message.data.data.tipo
+                        } else if (message.data.data.tipo == "Actividad") {
+                            $cordovaLocalNotification.schedule({
+                                id: id_message,
+                                title: 'Actividad',
+                                text: message.data.html,
+                                data: {
+                                    tipo: message.data.data.tipo
+                                }
+                            });
+                        } else if (message.data.data.tipo == "Respuesta") {
+                            if ($state.current.name == "app.respuestas" && message.data.data.reporte_id == parseInt($state.params.reporteId)) {
+                                scope.$broadcast('leer', message.data.data);
+                            } else {
+                                $cordovaLocalNotification.schedule({
+                                    id: id_message,
+                                    title: 'Respuesta',
+                                    text: message.data.html,
+                                    data: {
+                                        reporte: message.data.data.reporte_id,
+                                        tipo: message.data.data.tipo
+                                    }
+                                });
                             }
-                        });
-                    } else if (message.data.data.tipo == "Respuesta") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Respuesta',
-                            text: message.data.html,
-                            data: {
-                                reporte: message.data.data.reporte_id,
-                                tipo: message.data.data.tipo
+                        } else if (message.data.data.tipo == "Recordatorio") {
+                            $cordovaLocalNotification.schedule({
+                                id: id_message,
+                                title: 'Recordatorio',
+                                text: message.data.html,
+                                data: {
+                                    reporte: message.data.data.reporte_id,
+                                    tipo: message.data.data.tipo
+                                }
+                            });
+                        } else if (message.data.data.tipo == "Solucion") {
+                            if ($state.current.name == "app.historialM") {
+                                scope.$broadcast('leer', message.data.data);
+                            } else {
+                                $cordovaLocalNotification.schedule({
+                                    id: id_message,
+                                    title: 'Solución de Reporte',
+                                    text: message.data.html,
+                                    data: {
+                                        solucion: message.data.data.solucion_id,
+                                        reporte: message.data.data.reporte_id,
+                                        tipo: message.data.data.tipo
+                                    }
+                                });
                             }
-                        });
-                    } else if (message.data.data.tipo == "Recordatorio") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Recordatorio',
-                            text: message.data.html,
-                            data: {
-                                reporte: message.data.data.reporte_id,
-                                tipo: message.data.data.tipo
-                            }
-                        });
-                    } else if (message.data.data.tipo == "Solucion") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Solución de Reporte',
-                            text: message.data.html,
-                            data: {
-                                solucion: message.data.data.solucion_id,
-                                reporte: message.data.data.reporte_id,
-                                tipo: message.data.data.tipo
-                            }
-                        });
-                    } else if (message.data.data.tipo == "Asignacion") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Asignación',
-                            text: message.data.html,
-                            data: {
-                                asignacion: message.data.data.asignacion_id,
-                                tipo: message.data.data.tipo
-                            }
-                        });
-                    } else if (message.data.data.tipo == "Reporte informativo") {
-                        $cordovaLocalNotification.schedule({
-                            id: id_message,
-                            title: 'Reporte informativo',
-                            text: message.data.html,
-                            data: {
-                                actual: message.data.data.reporte_id,
-                                tipo: message.data.data.tipo
-                            }
-                        });
+                        } else if (message.data.data.tipo == "Asignacion") {
+                            $cordovaLocalNotification.schedule({
+                                id: id_message,
+                                title: 'Asignación',
+                                text: message.data.html,
+                                data: {
+                                    asignacion: message.data.data.asignacion_id,
+                                    tipo: message.data.data.tipo
+                                }
+                            });
+                        }
                     }
                     $rootScope.$on('$cordovaLocalNotification:click',
                         function(event, notification, state) {
@@ -149,17 +155,12 @@ angular.module('starter.socket', [])
                                     });
                                 } else if (data.tipo === "Asignacion") {
                                     $state.go('app.ruta', {
-                                      actual: data.asignacion
-                                    },
-                                    {
-                                        reload: true
-                                    });
-                                } else if (data.tipo == "Reporte informativo") {
-                                    $state.go('app.historialI', {
-                                        actual: data.reporte
+                                        actual: data.asignacion
                                     }, {
                                         reload: true
                                     });
+                                } else if (data.tipo === "Actividad") {
+                                    $state.go('app.notificaciones');
                                 }
                             });
                         }.bind(this));
@@ -168,9 +169,9 @@ angular.module('starter.socket', [])
 
             scope.socket.on('visited', function(message) {
                 var elemento = this.notixList.filter(function(element) {
-                  return element._id == message.message_id;
+                    return element._id == message.message_id;
                 });
-                if(elemento.length>0){
+                if (elemento.length > 0) {
                     var index = this.notixList.indexOf(elemento[0]);
                     if (index > -1) {
                         this.notixList.splice(index, 1);
@@ -230,6 +231,33 @@ angular.module('starter.socket', [])
                 "django_id": this.django_id,
                 "webuser": this.username
             });
+        },
+
+        limpiar: function(tipo) {
+            this.notixList.forEach(function(elemento) {
+                if(tipo=="Respuesta"){
+                  if (parseInt($state.params.reporteId) == elemento.data.data.reporte_id) {
+                    if(elemento.data.data.tipo == tipo) {
+                        this.visit(elemento._id);
+                    }
+                  }
+                }
+                else{
+                  if(elemento.data.data.tipo == tipo) {
+                      this.visit(elemento._id);
+                  }
+                }
+            }.bind(this));
+        },
+
+        leido: function() {
+            if ($state.current.name == "app.historialR") {
+                this.limpiar("Reporte");
+            } else if ($state.current.name == "app.respuestas") {
+                this.limpiar("Respuesta");
+            } else if ($state.current.name == "app.historialM") {
+                this.limpiar("Solucion");
+            }
         }
     };
 
